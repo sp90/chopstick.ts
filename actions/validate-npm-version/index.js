cfe11f49d14e82d2c731edac083649f00d43cada
Simon Dragsbæk <dev@nomis.dk>
2022-08-19 15:21:43 +0200

    Update action to match semver

--- actions/validate-npm-version/index.js
+++ actions/validate-npm-version/index.js
@@ -1,16 +1,15 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const { execSync } = require('child_process')
+const semver = require('semver')
 const packageJson = require('../../package.json')
 
 try {
   const packageName = core.getInput('package-name')
   const oldVersion = execSync(`npm show ${packageName} version`).toString()
-  console.log('Old version: ', oldVersion)
-
   const newVersion = packageJson.version
+  const isNewer = semver.gt(oldVersion, newVersion)
 
-  console.log('New version: ', newVersion)
+  isNewer ? core.setCommandEcho('Version is newer') : core.setFailed()
 } catch (error) {
   core.setFailed(error.message)
 }
