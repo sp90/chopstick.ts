22c3fc768af0e8994a4ba85613bd6552d5e6a746
Simon Dragsbæk <dev@nomis.dk>
2022-08-19 15:09:45 +0200

    Bake in version call to the custom validate action

--- actions/validate-npm-version/index.js
+++ actions/validate-npm-version/index.js
@@ -1,12 +1,16 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { execSync } = require('child_process')
 const packageJson = require('../../package.json')
 
 try {
-  const oldVersion = core.getInput('current-version')
+  const packageName = core.getInput('package-name')
+
+  const oldVersion = execSync(`npm show ${packageName} version`)
+  console.log('Old version: ', oldVersion)
+
   const newVersion = packageJson.version
 
-  console.log('Old version: ', oldVersion)
   console.log('New version: ', newVersion)
 } catch (error) {
   core.setFailed(error.message)
