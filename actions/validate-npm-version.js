34fc33f9e655c7494c9c459d90acbed14706d1a8
Simon Dragsbæk <dev@nomis.dk>
2022-08-19 14:56:53 +0200

    Test new deployment system

---
+++ actions/validate-npm-version.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const packageJson = require('../package.json')
+
+try {
+  const oldVersion = core.getInput('current-npm-version')
+  const newVersion = packageJson.version
+
+  console.log('Old version: ', oldVersion)
+  console.log('New version: ', newVersion)
+} catch (error) {
+  core.setFailed(error.message)
+}
+
+function breakVersion() {}
